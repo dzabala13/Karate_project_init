dcf24849f79f13ca6734b5804378380c8b2871ef
dzabala13 <danielisaaczabala@gmail.com>
2022-06-04 20:08:45 -0500

    manejo de reportes de Karate y de cucumber

--- src/test/java/users/ManagementTest.java
+++ src/test/java/users/ManagementTest.java
@@ -1,7 +1,17 @@
 package users;
 
+import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.transform.Result;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 public class ManagementTest {
@@ -9,6 +19,17 @@ public class ManagementTest {
     @Test
     void testParallel(){
 
-        Runner.path("classpath:users").tags("~@ignore").parallel(4);
+      Results results= Runner.path("classpath:users").outputCucumberJson(true).tags("~@ignore").parallel(4);
+      generateReports(results.getReportDir());
+    }
+
+    public static void generateReports( String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("build"), "KarateBuldingProject");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+
     }
 }
